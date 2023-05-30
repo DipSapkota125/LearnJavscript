27a0d9a6d7acbb9744366dda10f12f69ceb49bd2
DipSapkota125 <dipsapkota325@gmail.com>
2023-05-30 15:46:29 +0545

    add basic javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+const length = prompt("Enter length");
+const width = prompt("Enter width");
+const area = length * width;
+
+console.log(area);
+alert("The area of rectangle is :" + area);
+document.write("The area of rectangle is :" + area);
